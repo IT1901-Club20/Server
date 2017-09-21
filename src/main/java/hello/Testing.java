d884666d5a2d0f03ed074b4788f0aa6d9cdd6830
Erik Dymbe <erik.dymbe@gmail.com>
2017-09-21 08:37:09 +0200

    add build to repo

--- src/main/java/hello/Testing.java
+++ src/main/java/hello/Testing.java
@@ -1,7 +1,7 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +11,9 @@ public class Testing {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("hei")
-    public Greeting hei(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(), "hei");
+    @GetMapping("/hei")
+    public String hei() {
+        return "WTF DID YOU JUST SAY TO ME YOU PIECE OF SHIT?";
     }
 
 }
